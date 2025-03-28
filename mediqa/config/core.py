300bed6b9e59c17bb0bc9b0cba1f2eb8da9af8f8
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2025-03-28 15:30:33 -0500

    Develop preprocessing script

---
+++ mediqa/config/core.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+from pydantic import BaseModel
+from strictyaml import YAML, load
+from typing import Optional
+
+import mediqa
+
+# Project Directories
+PACKAGE_ROOT = Path(mediqa.__file__).resolve().parent
+ROOT = PACKAGE_ROOT.parent
+CONFIG_FILE_PATH = PACKAGE_ROOT / "config.yml"
+DATASET_DIR = PACKAGE_ROOT / "data"
+
+class DataConfig(BaseModel):
+    eval_src_file: str
+    split_frac: float
+    val_file: str
+    test_file: str
+
+class ModelConfig(BaseModel):
+    model_name: str
+
+class Config(BaseModel):
+    """Master config object."""
+    data_conf: DataConfig
+    model_conf: ModelConfig
+
+def find_config_file() -> Path:
+    if CONFIG_FILE_PATH.is_file():
+        return CONFIG_FILE_PATH
+    raise Exception(f"Config not found at {CONFIG_FILE_PATH}")
+
+def fetch_config_from_yaml(cfg_path: Optional[Path] = None) -> YAML:
+    """Parse YAML containing the package configuration"""
+    if cfg_path is None:
+        cfg_path = find_config_file()
+
+    if cfg_path:
+        with open(cfg_path, "r") as f:
+            parsed_config = load(f.read())
+            return parsed_config
+    raise OSError(f"Did not find config file at: {cfg_path}")
+
+def create_and_validate_config(parsed_config: YAML = None) -> Config:
+    if parsed_config is None:
+        parsed_config = fetch_config_from_yaml()
+
+    _config = Config(
+        data_conf=DataConfig(**parsed_config.data),
+        model_conf=ModelConfig(**parsed_config.data),
+    )
+
+    return _config
+
+config = create_and_validate_config()
---
+++ mediqa/processing/preprocess.py
@@ -0,0 +1,72 @@
+import pandas as pd
+from typing import Tuple
+
+from mediqa.config.core import config, DataConfig, DATASET_DIR
+
+def cleanup(df: pd.DataFrame) -> pd.DataFrame:
+    """Removes offending examples from the dataset.
+    Specifically, it removes samples with the following issues:
+    - NaNs
+    - Duplicate questions and/or answers
+
+    Args:
+        df (pd.DataFrame): Original dataset. `question` and `anser` columns are expected
+
+    Returns:
+        pd.DataFrame: Cleaned dataset.
+    """
+    # Remove NaNs
+    df = df.dropna()
+
+    # Remove duplicates
+    df = df.drop_duplicates(subset=["question"])
+    df = df.drop_duplicates(subset=["answer"])
+
+    return df
+
+def split(df: pd.DataFrame, frac=0.5) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Splits the dataset into validation and test sets.
+
+    Args:
+        df (pd.DataFrame): Original dataset. `question` and `answer` columns are expected
+
+    Returns:
+        Tuple[pd.DataFrame, pd.DataFrame]: Train and test datasets.
+    """
+    # Split the dataset into val and test sets
+    train_df = df.sample(frac=frac, random_state=42)
+    test_df = df.drop(train_df.index)
+
+    return train_df, test_df
+
+def preprocess(data_config: DataConfig) -> None:
+    """Preprocess the input dataset and saves it as validation and test sets
+
+    Args:
+        input_path (str): 
+        output_path (str): _description_
+    """
+    # Construct the relevant paths
+    input_path = DATASET_DIR / data_config.eval_src_file
+    val_path = DATASET_DIR / data_config.val_file
+    test_path = DATASET_DIR / data_config.test_file
+
+    # Load the dataset
+    df = pd.read_csv(input_path)
+
+    # Clean the dataset
+    df = cleanup(df)
+
+    # Split the dataset
+    val_df, test_df = split(df, frac=data_config.split_frac)
+
+    # Save the dataset
+    val_df.to_csv(val_path, index=False)
+    test_df.to_csv(test_path, index=False)
+
+if __name__ == "__main__":
+    # Load the configuration
+    data_config = config.data_conf
+
+    # Preprocess the dataset
+    preprocess(data_config)
